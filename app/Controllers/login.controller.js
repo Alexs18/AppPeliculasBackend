4de51bc72a1e3d9448bdac459fa96c61e91d51f7
Alexs18 <mclexis18@gmail.com>
2022-11-09 05:34:29 +0100

    Metodo de login y register

--- app/Controllers/login.controller.js
+++ app/Controllers/login.controller.js
@@ -1,34 +1,39 @@
 let Pool = require('../Database/index');
+let bcrypt = require('bcrypt')
 
 class Login{
     async login(req, res){
 
        let {password, email} = req.body;
        try {
-        let {rows} = await Pool.query('select * from users');
-        debugger
-       for (const usuarios of rows) {
-        debugger
-            if (usuarios.password === password && usuarios.email === email) {
-                res.status(200).
-                json({
-                    message:'el usuario esta logeado'
-                })
-            }
-            else{
-                res.status(404).
-                json({
-                    message:'el usuario no existe'
-                })
-            } 
-       }
+        console.log(`select * from users where email = '${email}'`);
+        let {rows} = await Pool.query(`select * from users where email = '${email}'`);
+        
+        if (rows[0].password === password) {
+            res.status(200).json({
+                message:`El usuario ${rows[0].nombre} se logió correctamente`,
+            })   
+        }else{
+            res.status(400).json({
+                message:`Las contraseñas no coinciden`,
+            })
+        }
+       
        } catch (error) {
         console.log(error)
        }
        
     }
-    logout(){
+    async Register(req, res){
+        let {nombre, apellido, password, email} = req.body;
+        let newpassword = await bcrypt.hash(password, 10);
 
+        let UserRegisted = await Pool.query(`insert into users (nombre, apellido, password, email) values($1, $2, $3, $4)
+        RETURNING id, password, nombre`, [nombre, apellido, newpassword, email])
+        res.status(200).json({
+            message:'usuario Registrado correctamente',
+            id:UserRegisted.rows
+        })
     }
 }
 
--- app/routes/Login.Routes.js
+++ app/routes/Login.Routes.js
@@ -1,7 +1,8 @@
 let Express = require('express');
 let Router = Express.Router();
-let {login} = require('../Controllers/login.controller')
+let {login, Register} = require('../Controllers/login.controller')
 
-Router.post('/login', login)
+Router.post('/login', login);
+Router.post('/register', Register)
 
 module.exports = Router;
